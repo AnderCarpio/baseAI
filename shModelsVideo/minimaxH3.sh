a9b9cda6d09039d4d63a8f499a74b47fae625c27
AnderCarpio <andercarpio1007@proton.me>
2026-08-30 15:52:46 -0600

    Change Huggenface Text Encoder Minimax H3

--- shModelsVideo/minimaxH3.sh
+++ shModelsVideo/minimaxH3.sh
@@ -5,9 +5,6 @@
 wget -c https://huggingface.co/Comfy-Org/MiniMax-H3/resolve/main/diffusion_models/minimax_h3_fl2va_pruned_int8_convrot.safetensors -P ComfyUI/models/diffusion_models
 wget -c https://huggingface.co/Comfy-Org/MiniMax-H3/resolve/main/diffusion_models/minimax_h3_ref2va_pruned_int8_convrot.safetensors -P ComfyUI/models/diffusion_models
 
-# Text Endocer
-wget -c https://huggingface.co/ethanfel/Qwen3-VL-32B-Ultra-Heretic-H3-ComfyUI-INT8-ConvRot/resolve/main/qwen3vl_32b_h3_ultra_uncensored_heretic_int8_convrot.safetensors -P ComfyUI/models/text_encoders
-
 # VAE
 wget -c https://huggingface.co/Comfy-Org/MiniMax-H3/resolve/main/vae/minimax_h3_audio_vae_fp32.safetensors -P ComfyUI/models/vae
 wget -c https://huggingface.co/Comfy-Org/MiniMax-H3/resolve/main/vae/minimax_h3_video_vae_fp16.safetensors -P ComfyUI/models/vae
@@ -18,8 +15,8 @@ wget -c https://huggingface.co/larryvrh/MiniMax-H3-Turbo-Lora/resolve/main/minim
 # Lora
 wget -c https://huggingface.co/model-hub-nd/test3/resolve/main/MysticXXX-v4.safetensors -P ComfyUI/models/loras
 
-
-
 # TAE
 wget -c https://huggingface.co/Kijai/MiniMax-H3-TAE/resolve/main/vae_approx/taeh3.safetensors -P ComfyUI/models/loras
 
+# Text Endocer
+wget -c https://huggingface.co/buckets/AnderCarpio07/Qwen3-VL-32B-Ultra-Heretic-H3-ComfyUI-INT8-ConvRot-bucket/resolve/qwen3vl_32b_h3_ultra_uncensored_heretic_int8_convrot.safetensors -P ComfyUI/models/text_encoders
